89cad9b3f584459d9cdff23320270d8aa7b3b832
hesham <heshamabdelazim3@gmail.com>
2024-06-19 00:06:34 +0300

    first commit

---
+++ js-test.js
@@ -0,0 +1,4 @@
+// console.log(document.styleSheets[2].rules[12].style.setProperty("background-color", "lightcyan", "important"));
+// document.styleSheets[2].rules[12].style.setProperty("background-color", "lightcyan", "important"); // to put a property in the style sheet
+// document.styleSheets[2].rules[12].style.removeProperty("display"); // to delete a property in the style sheet
+console.log(document.styleSheets[2]);
